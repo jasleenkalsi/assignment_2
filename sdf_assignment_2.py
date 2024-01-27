c6843d5c9a9c316119c209ad722b58832d856f59
jasleen <jasleenkaurkalsi03@gmail.com>
2024-01-27 14:29:32 -0600

    Simple data , calculations and lists.

---
+++ sdf_assignment_2.py
@@ -0,0 +1,23 @@
+#Jasleen kaur
+
+#SIMPLE DATA TYPES
+Name = "Jasleen"
+print(Name)
+ 
+Mantioba_Driving_licene = "No"
+print(Mantioba_Driving_licene)
+
+Current_year = "2024"
+print(Current_year)
+                
+
+#CALCULATIONS
+
+#LISTS
+
+#TUPLES
+
+#DICTIONARIES
+
+#SETS
+
