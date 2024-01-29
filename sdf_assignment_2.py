7a33e4157391306543c54e83b5aaa422ce6201b8
jasleen <jasleenkaurkalsi03@gmail.com>
2024-01-29 09:22:15 -0600

    Initialise the calculationd in different taxs.

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -12,7 +12,29 @@ print(Current_year)
                 
 
 #CALCULATIONS
+# constants for Federal Goods and Services Tax (GST)
+GST_RATE = 5
+print(f"Federal Tax (GST) at {GST_RATE}%")
 
+# constant for Manitoba Provisional tax (PST)
+PST_RATE = 7
+print(f"Provincial Tax (PST) at {PST_RATE}%")
+
+
+# variable for the price of the vehicle
+vehicle_price = 70000.00 
+print(f"Vehicle Price: ${vehicle_price:.2f}")
+
+
+vehicle_price = 70000.00
+PST_RATE = 5
+GST_RATE = 7
+total_value = vehicle_price + PST_RATE + GST_RATE
+
+print(f"vehicle_price: {vehicle_price}\nProvincial Tax: { PST_RATE}\nFederal Tax: {GST_RATE}\nTotal: {total_value}")
+
+
+# Print the results in the specified format using a single print statement
 #LISTS
 
 #TUPLES
