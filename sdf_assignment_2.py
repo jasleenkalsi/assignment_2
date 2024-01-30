aa6c79f531fc9a3af71ff810fbfa8ce19004fe46
jasleen <jasleenkaurkalsi03@gmail.com>
2024-01-29 19:25:45 -0600

    Initialise and Print data , calculations , lists , tuples and sets.

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -71,6 +71,54 @@ print(canadian_provinces)
 print(type(canadian_provinces))
 
 #DICTIONARIES
+#  initialize a dictionary with keys 'nickel', 'dime', and 'quarter'
+canadian_currency = {'nickel': 0.05, 'dime': 0.10, 'quarter': 0.25}
+print(canadian_currency)
+
+
+# Verify the data type of the dictionary
+print(type(canadian_currency))
+
+
+# Modify the values to use whole numbers
+canadian_currency['nickel'] = 5
+canadian_currency['dime'] = 10
+canadian_currency['quarter'] = 25
+print(canadian_currency)
+
+# Add two items to the dictionary representing the loonie and toonie
+canadian_currency['loonie'] = 100
+canadian_currency['toonie'] = 200
+print(canadian_currency)
+
 
 #SETS
+
+# A set containing even numbers between 2 and 20 inclusive
+even_numbers_set = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+print(even_numbers_set)
+
+# Verify the data type of the set
+print(type(even_numbers_set))
+
+# A set containing multiples of 5 between 5 and 20 inclusive
+multiples_of_5_set = {5, 10, 15, 20}
+print(multiples_of_5_set)
+
+# Declare a set containing each unique value of the two sets created above
+unique_values_set = even_numbers_set.union(multiples_of_5_set)
+print(unique_values_set)
+
+# A set containing only those values that appear in both sets
+intersection_set = even_numbers_set.intersection(multiples_of_5_set)
+print(intersection_set)
+
+# A set containing only those values that appear in the first set but not in the second set
+difference_set_1 = even_numbers_set.difference(multiples_of_5_set)
+print(difference_set_1)
+
+# A set containing only those values that appear in the second set but not in the first set
+difference_set_2 = multiples_of_5_set.difference(even_numbers_set)
+print(difference_set_2)
+
 
